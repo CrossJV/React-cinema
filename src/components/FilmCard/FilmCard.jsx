4c2129f67ed063724da48767f8a7df6a1965a3cc
CrossJV <crossjvj@gmail.com>
2024-02-19 22:40:27 +0300

    refactor

--- src/components/FilmCard/FilmCard.jsx
+++ src/components/FilmCard/FilmCard.jsx
@@ -4,24 +4,28 @@ import Rating from '../Rating/Rating';
 import styles from './FilmCard.module.css';
 
 function FilmCard({ data }) {
-	return (
-		<li className={styles['film-card']}>
-			<div className={styles['poster-wrapper']}>
-				<Rating rating={data.rating || 0} />
-				<img className={styles['poster-image']} src={data.poster} alt={data.name || 'HZ FILM'} />
-			</div>
-			<div className={styles['description-wrapper']}>
-				<Paragraph 
-					text={data.name || 'Нетзвестный фильм'}
-					className={'film-name'}
-				/>
-				<Favorite 
-					isFavorite={data.favorites}
-					onClick={() => console.log('favorite push')}
-				/>
-			</div>
-		</li>
-	);
+	if(data)
+	{
+		return (
+			<li className={styles['film-card']}>
+				<div className={styles['poster-wrapper']}>
+					<Rating rating={data.rating || 0} />
+					<img className={styles['poster-image']} src={data.poster} alt={data.name || 'HZ FILM'} />
+				</div>
+				<div className={styles['description-wrapper']}>
+					<Paragraph 
+						text={data.name || 'Нетзвестный фильм'}
+						className={'film-name'}
+					/>
+					<Favorite 
+						isFavorite={data.favorites}
+						onClick={() => console.log('favorite push')}
+					/>
+				</div>
+			</li>
+		);
+	}
+	
 }
 
 export default FilmCard;
